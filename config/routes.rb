f49f1dfaea02d528acb799ebf3c528df625c39f2
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-03 16:04:19 -0700

    changes handrolled routes to use specific slug names, adds :slug to nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,31 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
-  resources :items, only: [:index, :show], param: :slug
+  resources :items, only: [:index, :show], param: :item_slug
   resources :merchants, only: [:index]
 
   get '/cart', to: 'cart#index'
-  post '/cart/additem/:id', to: 'cart#add_item', as: 'cart_add_item'
-  post '/cart/addmoreitem/:id', to: 'cart#add_more_item', as: 'cart_add_more_item'
+  post '/cart/additem/:item_slug', to: 'cart#add_item', as: 'cart_add_item'
+  post '/cart/addmoreitem/:item_slug', to: 'cart#add_more_item', as: 'cart_add_more_item'
   delete '/cart', to: 'cart#destroy', as: 'cart_empty'
-  delete '/cart/item/:id', to: 'cart#remove_more_item', as: 'cart_remove_more_item'
-  delete '/cart/item/:id/all', to: 'cart#remove_all_of_item', as: 'cart_remove_item_all'
+  delete '/cart/item/:item_slug', to: 'cart#remove_more_item', as: 'cart_remove_more_item'
+  delete '/cart/item/:item_slug/all', to: 'cart#remove_all_of_item', as: 'cart_remove_item_all'
 
   get '/login', to: 'session#new'
   post '/login', to: 'session#create'
   get '/logout', to: 'session#destroy'
 
   get '/register', to: 'users#new', as: 'registration'
-  resources :users, only: [:create, :update]
+  resources :users, only: [:create, :update], param: :user_slug
 
   get '/dashboard', to: 'merchants#show', as: 'dashboard'
   namespace :dashboard do
     resources :orders, only: [:show] do
-      patch '/items/:id/fulfill', to: 'orders#fulfill_item', as: 'item_fulfill'
+      patch '/items/:item_slug/fulfill', to: 'orders#fulfill_item', as: 'item_fulfill'
     end
-    resources :items, except: [:show]
-    patch '/items/:id/enable', to: 'items#enable', as: 'enable_item'
-    patch '/items/:id/disable', to: 'items#disable', as: 'disable_item'
+    resources :items, except: [:show], param: :item_slug
+    patch '/items/:item_slug/enable', to: 'items#enable', as: 'enable_item'
+    patch '/items/:item_slug/disable', to: 'items#disable', as: 'disable_item'
   end
   get '/profile', to: 'profile#index', as: 'profile'
 
@@ -34,20 +34,21 @@ Rails.application.routes.draw do
     resources :orders, only: [:index, :create, :show, :destroy]
   end
 
-  post '/admin/users/:merchant_id/items', to: 'dashboard/items#create', as: 'admin_user_items'
-  patch '/admin/users/:merchant_id/items/:id', to: 'dashboard/items#update', as: 'admin_user_item'
+  post '/admin/users/:merchant_slug/items', to: 'dashboard/items#create', as: 'admin_user_items'
+  patch '/admin/users/:merchant_slug/items/:item_slug', to: 'dashboard/items#update', as: 'admin_user_item'
+
   namespace :admin do
-    resources :users, only: [:index, :show, :edit] do
+    resources :users, only: [:index, :show, :edit], param: :slug do
       patch '/enable', to: 'users#enable', as: 'enable'
       patch '/disable', to: 'users#disable', as: 'disable'
       patch '/upgrade', to: 'users#upgrade', as: 'upgrade'
       resources :orders, only: [:index, :show]
     end
-    resources :merchants, only: [:show] do
+    resources :merchants, only: [:show], param: :slug do
       patch '/enable', to: 'merchants#enable', as: 'enable'
       patch '/disable', to: 'merchants#disable', as: 'disable'
       patch '/upgrade', to: 'merchants#downgrade', as: 'downgrade'
-      resources :items, only: [:index, :new, :edit]
+      resources :items, only: [:index, :new, :edit], param: :item_slug
     end
     resources :dashboard, only: [:index]
   end

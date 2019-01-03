ee0bed1dc2d9681523f941bfd9af229aa88d79bd
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-03 10:01:24 -0700

    changes finding merchant from params[:slug] to params[:merchant_slug]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,19 @@
 class Admin::ItemsController < Admin::BaseController
   def index
-    @merchant = User.find_by(slug: params[:slug])
+    @merchant = User.find_by(slug: params[:merchant_slug])
     @items = @merchant.items
     render '/dashboard/items/index'
   end
 
   def new
-    @merchant = User.find_by(slug: params[:slug])
+    @merchant = User.find_by(slug: params[:merchant_slug])
     @item = Item.new
     @form_path = [:admin, @merchant, @item]
     render "/dashboard/items/new"
   end
 
   def edit
-    @merchant = User.find_by(slug: params[:slug])
+    @merchant = User.find_by(slug: params[:merchant_slug])
     @item = Item.find_by(slug: params[:slug])
     @form_path = [:admin, @merchant, @item]
     render "/dashboard/items/edit"

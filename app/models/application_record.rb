117b34db7d0f26f3d77bdf0c8bcf894a65cffe97
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-02 18:27:34 -0700

    adds before_save and generate_slug method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  before_save :generate_slug
+
+  private
+
+  def generate_slug
+    return false unless self.class == Item
+    self.slug = "#{name.downcase.gsub(" ", "-")}#{SecureRandom.hex(3)}" if name
+  end
 end

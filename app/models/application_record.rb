042b15a7e457b240f5ed620a5e16ee0f6477a735
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-03 16:01:52 -0700

    adds checking for existing slug - updating found_item.slug to start with 0, new one start with 1

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -8,6 +8,26 @@ class ApplicationRecord < ActiveRecord::Base
     return false unless self.class == Item || self.class == User
 
     self.slug = email.parameterize.gsub("_", "-") if self.class == User
-    self.slug = name.parameterize if self.class == Item
+    if self.class == Item
+      slug = name.parameterize
+      self.slug = check_for_existing_slug(slug)
+    end
+  end
+
+  def check_for_existing_slug(slug)
+    found_item = Item.find_by(slug: slug)
+    return self.slug if found_item == self
+
+    if found_item
+      if found_item.slug[0] =~ /\d/
+        return "#{found_item.slug[0].to_i + 1}#{found_item.slug[1..-1]}"
+      else
+        found_item.update(slug: "0-#{found_item.slug}")
+        return "1-#{slug}"
+      end
+    else
+      return self.slug if self.slug
+      return slug
+    end
   end
 end

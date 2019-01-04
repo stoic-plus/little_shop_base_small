dcb1b1a47d0e2f6e0f888f54b356c234d222d48d
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-03 18:11:11 -0700

    removes updating first with 0- for existing slug on an item

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -11,23 +11,22 @@ class ApplicationRecord < ActiveRecord::Base
       self.slug = email.parameterize.gsub("_", "-") if email
     else
       slug = name.parameterize
+      binding.pry
       self.slug = check_for_existing_slug(slug)
     end
   end
 
   def check_for_existing_slug(slug)
     found_item = Item.find_by(slug: slug)
-    return self.slug if found_item == self
+    return slug if found_item == self
 
     if found_item
       if found_item.slug[0] =~ /\d/
         return "#{found_item.slug[0].to_i + 1}#{found_item.slug[1..-1]}"
       else
-        found_item.update(slug: "0-#{found_item.slug}")
         return "1-#{slug}"
       end
     else
-      return self.slug if self.slug
       return slug
     end
   end

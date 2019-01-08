b59ed2c5e42c8610e8d1eed1415123c8092977e1
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-08 09:39:03 -0700

    fixes typos

--- spec/features/users_see_merchant_fulfillment_stats_spec.rb
+++ spec/features/users_see_merchant_fulfillment_stats_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'More Merchant Stats - Fulfillment Speed by City/State' do
   before :each do
-    user_1 = create(:user, city: 'Springfield', state: 'CO')
+    @user_1 = create(:user, city: 'Springfield', state: 'CO')
     user_2 = create(:user, state: 'CO')
 
     @m1 = create(:merchant)
@@ -19,22 +19,22 @@ describe 'More Merchant Stats - Fulfillment Speed by City/State' do
     item_5 = create(:item, user: @m5)
     item_6 = create(:item, user: @m6)
 
-    order_1_a = create(:completed_order, user: user_1)
+    order_1_a = create(:completed_order, user: @user_1)
     oi_1_a = create(:fulfilled_order_item, order: order_1_a, item: item_1, quantity: 10, created_at: 12.days.ago, updated_at: 1.days.ago)
 
-    order_2_a = create(:completed_order, user: user_1)
+    order_2_a = create(:completed_order, user: @user_1)
     oi_2_a = create(:fulfilled_order_item, order: order_2_a, item: item_2, quantity: 10, created_at: 11.days.ago, updated_at: 1.days.ago)
 
-    order_3_a = create(:completed_order, user: user_1)
+    order_3_a = create(:completed_order, user: @user_1)
     oi_3_a = create(:fulfilled_order_item, order: order_3_a, item: item_3, quantity: 10, created_at: 10.days.ago, updated_at: 1.days.ago)
 
-    order_4_a = create(:completed_order, user: user_1)
+    order_4_a = create(:completed_order, user: @user_1)
     oi_4_a = create(:fulfilled_order_item, order: order_4_a, item: item_4, quantity: 10, created_at: 9.days.ago, updated_at: 1.days.ago)
 
-    order_5_a = create(:completed_order, user: user_1)
+    order_5_a = create(:completed_order, user: @user_1)
     oi_5_a = create(:fulfilled_order_item, order: order_5_a, item: item_5, quantity: 10, created_at: 8.days.ago, updated_at: 1.days.ago)
 
-    order_6_a = create(:completed_order, user: user_1)
+    order_6_a = create(:completed_order, user: @user_1)
     oi_6_a = create(:fulfilled_order_item, order: order_6_a, item: item_6, quantity: 10, created_at: 7.days.ago, updated_at: 1.days.ago)
 
 
@@ -56,8 +56,8 @@ describe 'More Merchant Stats - Fulfillment Speed by City/State' do
     order_6_b = create(:completed_order, user: user_2)
     oi_6_b = create(:fulfilled_order_item, order: order_6_b, item: item_3, quantity: 10, created_at: 1.days.ago, updated_at: 1.hours.ago)
 
-    @top_5_city = user_1.top_5_merchants_item_fulfillment_speed_city
-    @top_5_state = user_1.top_5_merchants_item_fulfillment_speed_state
+    @top_5_city = @user_1.top_5_merchants_item_fulfillment_speed_city
+    @top_5_state = @user_1.top_5_merchants_item_fulfillment_speed_state
   end
   context 'as a logged in user' do
     it 'shows top_5 merchants for state and city' do
@@ -68,24 +68,26 @@ describe 'More Merchant Stats - Fulfillment Speed by City/State' do
       top_5_merchant_ids = @top_5_state.map {|m| m.merchant_id}
       top_merch = User.get_merchant_info(top_5_merchant_ids)
 
-      within ".top_5_stats" do
-        within '.top_5_state' do
-          expect(all('.top_merchant_state')[0]).to have_content("Merchant: #{(top_merch.select{|m| m.first = top_5_merchant_ids[0]}).last}, Fulfillment Time: #{@top_5_state[0].fulfill_speed}")
-          expect(all('.top_merchant_state')[1]).to have_content("Merchant: #{(top_merch.select{|m| m.first = top_5_merchant_ids[1]}).last}, Fulfillment Time: #{@top_5_state[1].fulfill_speed}")
-          expect(all('.top_merchant_state')[2]).to have_content("Merchant: #{(top_merch.select{|m| m.first = top_5_merchant_ids[2]}).last}, Fulfillment Time: #{@top_5_state[2].fulfill_speed}")
-          expect(all('.top_merchant_state')[3]).to have_content("Merchant: #{(top_merch.select{|m| m.first = top_5_merchant_ids[3]}).last}, Fulfillment Time: #{@top_5_state[3].fulfill_speed}")
-          expect(all('.top_merchant_state')[4]).to have_content("Merchant: #{(top_merch.select{|m| m.first = top_5_merchant_ids[4]}).last}, Fulfillment Time: #{@top_5_state[4].fulfill_speed}")
+      within ".top-5-stats" do
+        within '.top-5-state' do
+          expect(page).to have_content("In #{@user_1.state}: ")
+          expect(all('.top-merchant-state')[0]).to have_content("Merchant: #{(top_merch.select{|m| m.first == top_5_merchant_ids[0]}).last}, Fulfillment Time: #{@top_5_state[0].fulfill_speed}")
+          expect(all('.top-merchant-state')[1]).to have_content("Merchant: #{(top_merch.select{|m| m.first == top_5_merchant_ids[1]}).last}, Fulfillment Time: #{@top_5_state[1].fulfill_speed}")
+          expect(all('.top-merchant-state')[2]).to have_content("Merchant: #{(top_merch.select{|m| m.first == top_5_merchant_ids[2]}).last}, Fulfillment Time: #{@top_5_state[2].fulfill_speed}")
+          expect(all('.top-merchant-state')[3]).to have_content("Merchant: #{(top_merch.select{|m| m.first == top_5_merchant_ids[3]}).last}, Fulfillment Time: #{@top_5_state[3].fulfill_speed}")
+          expect(all('.top-merchant-state')[4]).to have_content("Merchant: #{(top_merch.select{|m| m.first == top_5_merchant_ids[4]}).last}, Fulfillment Time: #{@top_5_state[4].fulfill_speed}")
         end
 
         top_5_merchant_ids = @top_5_city.map {|m| m.merchant_id}
         top_merch = User.get_merchant_info(top_5_merchant_ids)
 
-        within 'top_5_city' do
-          expect(all('.top_merchant_city')[0]).to have_content("Merchant: #{(top_merch.select{|m| m.first = top_5_merchant_ids[0]}).last}, Fulfillment Time: #{@top_5_city[0].fulfill_speed}")
-          expect(all('.top_merchant_city')[1]).to have_content("Merchant: #{(top_merch.select{|m| m.first = top_5_merchant_ids[1]}).last}, Fulfillment Time: #{@top_5_city[1].fulfill_speed}")
-          expect(all('.top_merchant_city')[2]).to have_content("Merchant: #{(top_merch.select{|m| m.first = top_5_merchant_ids[2]}).last}, Fulfillment Time: #{@top_5_city[2].fulfill_speed}")
-          expect(all('.top_merchant_city')[3]).to have_content("Merchant: #{(top_merch.select{|m| m.first = top_5_merchant_ids[3]}).last}, Fulfillment Time: #{@top_5_city[3].fulfill_speed}")
-          expect(all('.top_merchant_city')[4]).to have_content("Merchant: #{(top_merch.select{|m| m.first = top_5_merchant_ids[4]}).last}, Fulfillment Time: #{@top_5_city[4].fulfill_speed}")
+        within '.top-5-city' do
+          expect(page).to have_content("In #{@user_1.city}, #{@user_1.state}: ")
+          expect(all('.top-merchant-city')[0]).to have_content("Merchant: #{(top_merch.select{|m| m.first == top_5_merchant_ids[0]}).last}, Fulfillment Time: #{@top_5_city[0].fulfill_speed}")
+          expect(all('.top-merchant-city')[1]).to have_content("Merchant: #{(top_merch.select{|m| m.first == top_5_merchant_ids[1]}).last}, Fulfillment Time: #{@top_5_city[1].fulfill_speed}")
+          expect(all('.top-merchant-city')[2]).to have_content("Merchant: #{(top_merch.select{|m| m.first == top_5_merchant_ids[2]}).last}, Fulfillment Time: #{@top_5_city[2].fulfill_speed}")
+          expect(all('.top-merchant-city')[3]).to have_content("Merchant: #{(top_merch.select{|m| m.first == top_5_merchant_ids[3]}).last}, Fulfillment Time: #{@top_5_city[3].fulfill_speed}")
+          expect(all('.top-merchant-city')[4]).to have_content("Merchant: #{(top_merch.select{|m| m.first == top_5_merchant_ids[4]}).last}, Fulfillment Time: #{@top_5_city[4].fulfill_speed}")
         end
       end
     end
